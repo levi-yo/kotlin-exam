da3dd63e8f061ebfcc02955c505c83abd92de86f
levi.yoon <levi.yoon@kakaocorp.com>
2020-09-29 22:58:36 +0900

    Adding coroutine example and opentracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("io.opentracing.contrib:opentracing-spring-jaeger-cloud-starter:2.0.3")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
--- src/main/kotlin/com/example/kotlin/controller/SampleController.kt
+++ src/main/kotlin/com/example/kotlin/controller/SampleController.kt
@@ -1,14 +1,14 @@
 package com.example.kotlin.controller
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import io.opentracing.Tracer
+import kotlinx.coroutines.*
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SampleController(val service: SampleService) {
+class SampleController(val service: SampleService, val tracer: Tracer) {
     @GetMapping("/")
     suspend fun index(): SampleEntity {
         println("index() current thread name = ${Thread.currentThread().name}")
@@ -25,7 +25,54 @@ class SampleController(val service: SampleService) {
         val age = async {
             service.getAge()
         }
+
         SampleEntity(name.await(), age.await())
+    }
+
+    /**
+     * 같은 코루틴 스콥에서 서로 다른 비동기 작업처리
+     */
+    @GetMapping("/async2")
+    suspend fun asyncRequest2(): SampleEntity {
+        println("asyncRequest2() current thread name = ${Thread.currentThread().name}")
+
+        val scope = CoroutineScope(Dispatchers.IO)
+
+        val name = scope.async {
+            service.getName()
+        }
+        val age = scope.async {
+            service.getAge()
+        }
+
+        return SampleEntity(name = name.await(), age = age.await())
+    }
+
+    /**
+     * 다른 코루틴 스콥에서 서로 다른 비동기 작업처리.
+     * 각 비동기 작업은 다른 코루틴에서 실행된다.
+     */
+    @GetMapping("/async3")
+    suspend fun asyncRequest3(): SampleEntity {
+        println("asyncRequest2() current thread name = ${Thread.currentThread().name}")
+
+        val name = CoroutineScope(Dispatchers.IO).async {
+            service.getName()
+        }
+        val age = CoroutineScope(Dispatchers.IO).async {
+            service.getAge()
+        }
+
+        return SampleEntity(name = name.await(), age = age.await())
+    }
+
+    @GetMapping("/launch")
+    suspend fun launchRequest(): SampleEntity {
+        CoroutineScope(Dispatchers.IO).launch {
+            println("launchRequest() current thread name = ${Thread.currentThread().name}")
+        }
+
+        return service.findEntity()
     }
 }
 
@@ -50,7 +97,7 @@ class SampleService(val repository: SampleRepository) {
 @Repository
 class SampleRepository {
     suspend fun findSample(): SampleEntity {
-        println("current thread name = ${Thread.currentThread().name}")
+        println("findSample() current thread name = ${Thread.currentThread().name}")
         return SampleEntity("levi", 29)
     }
 }
