89ae03f97a4ce243c01ea762b70bfac9cecd470c
levi.yoon <levi.yoon@kakaocorp.com>
2020-09-29 17:51:12 +0900

    apply async operation

--- src/main/kotlin/com/example/kotlin/controller/SampleController.kt
+++ src/main/kotlin/com/example/kotlin/controller/SampleController.kt
@@ -1,5 +1,7 @@
 package com.example.kotlin.controller
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,15 +11,39 @@ import org.springframework.web.bind.annotation.RestController
 class SampleController(val service: SampleService) {
     @GetMapping("/")
     suspend fun index(): SampleEntity {
+        println("index() current thread name = ${Thread.currentThread().name}")
         return service.findEntity()
+    }
+
+    @GetMapping("/async")
+    suspend fun asyncRequest(): SampleEntity = coroutineScope {
+        println("asyncRequest() current thread name = ${Thread.currentThread().name}")
+
+        val name = async {
+            service.getName()
+        }
+        val age = async {
+            service.getAge()
+        }
+        SampleEntity(name.await(), age.await())
     }
 }
 
 @Service
 class SampleService(val repository: SampleRepository) {
     suspend fun findEntity(): SampleEntity {
-        println("current thread name = ${Thread.currentThread().name}")
+        println("findEntity() current thread name = ${Thread.currentThread().name}")
         return repository.findSample()
+    }
+
+    suspend fun getName(): String {
+        println("getName() current thread name = ${Thread.currentThread().name}")
+        return "sora"
+    }
+
+    suspend fun getAge(): Int {
+        println("getAge() current thread name = ${Thread.currentThread().name}")
+        return 31
     }
 }
 
